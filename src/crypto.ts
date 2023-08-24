f90ed2dd77cfb9f74316a293cda492abb080b1c0
pythonSnake5036 <118955679+pythonSnake5036@users.noreply.github.com>
2023-08-24 16:48:15 -0700

    Initial commit

---
+++ src/crypto.ts
@@ -0,0 +1,14 @@
+/**
+ * Provides cryptography related functions
+ */
+//% weight=70 icon="\uf185" color=#EC7505
+// @ts-ignore
+namespace crypto {
+    /**
+     * Calculate a SHA256 checksum
+     */
+    //% blockId=tropic_pick block="pick a %fruit"
+    export function sha256(str: string): string {
+        return "";
+    }
+}
